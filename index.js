93cb182ed187d487e71c8f6c958d9c07b232df2d
Hristian Todorov <48565541+1freshie@users.noreply.github.com>
2020-10-03 20:58:41 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-var url = 'https://api.covidtracking.com/v1/us/daily.json';
+var url = 'https://api.covidtracking.com/v1/us/daily.json'; //A local page
 
 function load(url) {
   var xhr = new XMLHttpRequest();
 
   xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4) {
-      console.log(xhr.response);
+    if (xhr.readyState === 4 && xhr.status == 200) {
+      var dataGet = JSON.parse(xhr.responseText);
+      document.getElementById("TotalInfected").innerHTML = dataGet.death;
     }
   }
 
   xhr.open('GET', url, true);
   xhr.send('');
 }
-console.log("text");
-
-window.setInterval(function(){
-    load(url)
-  }, 10000);
+load(url);
