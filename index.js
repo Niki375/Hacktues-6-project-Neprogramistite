054e371cecd3a6697d863f0e9cccddfb2ab9d85e
Andrey Ivanov <72258883+Andreychooo@users.noreply.github.com>
2020-10-04 17:43:16 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
-var url = 'https://api.covidtracking.com/v1/us/daily.json'; //A local page
+var url = 'https://api.covidtracking.com/v1/us/daily.json'; 
 
 function load(url) {
   var xhr = new XMLHttpRequest();
 
   xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4) {
-      console.log(xhr.response);
+    if (xhr.readyState === 4 && xhr.status == 200) {
+      var dataGet = JSON.parse(xhr.response);
+      document.getElementById("TotalDeaths").innerHTML = dataGet[0].death;
+      document.getElementById("TotalInfected").innerHTML = dataGet[0].positive;
+      document.getElementById("TotalRecovered").innerHTML = dataGet[0].recovered;
+      document.getElementById("InfectedPerDay").innerHTML = dataGet[188].positive;
+      document.getElementById("DeathsPerDay").innerHTML = dataGet[188].death;
+      document.getElementById("RecoveredPerDay").innerHTML = dataGet[188].recovered;
     }
   }
 
   xhr.open('GET', url, true);
   xhr.send('');
 }
-console.log("text");
-load(url);
+
+load(url)
+
+window.setInterval(function(){
+    load(url)
+  }, 10000);
+
+
+
+
